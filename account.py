a89317199b10032311cc1c1e1a559a52db66c61f
akgm6 <78575493+akgm6@users.noreply.github.com>
2022-03-09 00:55:08 -0500

    adding code for account.py

---
+++ account.py
@@ -0,0 +1,38 @@
+lass Account:
+  """
+  This class makes up and operates the user account of the website. 
+  It accepts a user's username, password, email address and birth month and year.
+  This class does not accpect incorrect datatypes (e.g. an integer for a user name or a string for the birth month) and incorect value types (e.g. the user has to be born before 2005)
+  """
+  
+  def __init__(self, user_name, password, email, month, year):
+    if type(user_name) != str:
+      raise TypeError("Name must be a string!")
+    else:
+      self.user_name = user_name
+      
+    if type(password) != str:
+      raise TypeError("Password must be a string!")
+    else:
+      self.password = password
+      
+    if type(email) != str:
+      raise TypeError("Email must be a string!")
+    else:
+      self.email = email
+      
+    if type(month) != int:
+      raise TypeError("Birth month has to be an integer")
+    elif type(year) != int:
+      raise TypeError("Birth year has to be an integer")
+    elif year < 2005:
+      raise ValueError("You have to be born after 2005")
+    elif year <= 0 or month <= 0:
+      raise ValueError("Year cannot be a negative integer")
+    elif month > 12:
+      raise ValueError("Month does not exist")
+    elif year > 1900:
+      raise ValueError("Year does not exist")
+    else:
+      self.month = month
+      self.year = year
