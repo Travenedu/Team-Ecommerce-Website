ffac5ef4d37eb94a4448b11e0398e137079baf8d
akgm6 <78575493+akgm6@users.noreply.github.com>
2022-04-02 19:30:53 -0400

    combining the app.py files

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import render_template
 from flask import request, redirect, session, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-import bcrypt
-import secrets
+from company_information import item_dict 
+# import secrets
 import os
 
 app = Flask(__name__)
@@ -14,7 +14,6 @@ app = Flask(__name__)
 app.config['MONGO_URI'] = "mongodb+srv://Abbymercy:"+os.environ.get('ABBY_PWD')+"@cluster0.a5hk8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 mongo = PyMongo(app)
-
 
 app.secret_key = secrets.token_urlsafe(16)
 
@@ -37,7 +36,6 @@ def signup():
       birth_year = request.form['year']
       username = request.form['user_name']
       passworD = request.form['password']
-      
       
       salt = bcrypt.gensalt()
       hashed = bcrypt.hashpw(passworD, salt)
@@ -70,3 +68,33 @@ def login():
 def logout(userID):
   session.clear()
   return redirect('/')
+
+#Route Section
+@app.route('/')
+@app.route('/index')
+def index():
+    product_collection = mongo.db.Head2Toe
+    products = product_collection.find({})
+    return render_template('index.html', products=products)
+
+@app.route('/seed')
+def seed():
+    product_collection = mongo.db.Head2Toe
+    product_collection.insert_many(item_dict)
+    return "OK"
+
+@app.route('/item/<item_id>')
+def product(item_id):
+    print("Hello SDS")
+    print(item_id)
+    product_collection = mongo.db.Head2Toe
+    product = product_collection.find_one({"_id":ObjectId(item_id)})
+    print(product)
+    return render_template('product.html', product=product)
+
+
+@app.route('/session/<item_id>')
+def addto_cart(user_id):
+     user = mongo.db.user
+
+     product_collection = mongo.db.Head2Toe
