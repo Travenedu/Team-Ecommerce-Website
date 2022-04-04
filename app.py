29d3510a86cd25527ce82c0f1b66c67bac1aa412
Traven Reese <82552048+Travenedu@users.noreply.github.com>
2022-04-04 02:29:11 -0400

    Cleaning things Up
    Reorganizing

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import render_template
 from flask import request, redirect, session, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from company_information import item_dict 
-# import secrets
+from company_information import item_dict, product_types 
+import bcrypt
+import secrets
 import os
 
 app = Flask(__name__)
@@ -98,3 +99,62 @@ def addto_cart(user_id):
      user = mongo.db.user
 
      product_collection = mongo.db.Head2Toe
+
+@app.route('/inventory')
+def inventory():
+    #collection = mongo.db.Head2Toe
+    #if session:
+    #    pass
+        #user = session['username']
+        #user_ID = collection.find_one({}) 
+        #job_title = collection.find_one({})
+        #employee_image = collection.find_one({})
+    #else:
+    #untill everything is connected
+    user = "guest"
+    user_ID = 123
+    job_title = "Unavailble"
+    return render_template('Inventory.html', user = user, user_ID = user_ID, job_title = job_title)
+
+#This route is to look at the item individually 
+@app.route('/item/<itemID>')
+def item(itemID):
+    collection = mongo.db.Head2Toe
+    item = collection.find_one({"_id":ObjectId(itemID)})
+    return render_template('Inventory_item.html', item=item) 
+
+#This route is to add an item to inventory 
+@app.route('/new', methods=['GET', 'POST'])
+def new_inventory_item():
+    if request.method == 'GET':
+        return render_template('new_item.html', TypeofProduct=product_types)
+
+    else:
+        name = request.form['name'] 
+        price = request.form['price'] 
+        stock = request.form['stock'] 
+        TypeofProduct = request.form['TypeofProduct']
+        collection = mongo.db.Head2Toe
+        collection.insert_one({"name":name, "price":price, "stock":stock, "TypeofProduct":TypeofProduct})
+        return redirect('/inventory_view')
+
+#This route is to view the products in inventory
+@app.route('/inventory_view')
+def inventory_view():
+    collection = mongo.db.Head2Toe
+    products = collection.find({})
+    return render_template('inventory_all_items.html', products=products, Type_of_Product=product_types, label="All")
+
+@app.route('/product_type_view/<product_type>')
+def product_type_view(product_type):
+    collection = mongo.db.Head2Toe
+    products = collection.find({"Type of Product":product_type})
+    product_type = product_type.capitalize()
+    return render_template('inventory_all_items.html', products=products, Type_of_Product=product_type, label=product_type)
+
+@app.route('/item/<itemID>/delete')
+def delete(itemID):
+    collection = mongo.db.Head2Toe
+    item = collection.find_one({"_id":ObjectId(itemID)})
+    collection.delete_one(item)
+    return redirect('/inventory_view')
--- company_information.py
+++ company_information.py
@@ -1,3 +1,5 @@
+
+
 item_dict = [{"name":"Jergens Smoothing Aloe", "price":5.99, "stock":100, "Type of Product": "Lotion", "image": "https://kao-h.assetsadobe3.com/is/image/content/dam/sites/kaousa/www-jergens-com/images/master/products/daily-moisturizers/JER-CC-Hands-3oz-400x500.png?fmt=png-alpha&wid=320"}, 
              {"name":"Jergens Cloud Creme", "price":7.00, "stock":100, "Type of Product": "Lotion", "image": "https://kao-h.assetsadobe3.com/is/image/content/dam/sites/kaousa/www-jergens-com/images/master/products/daily-moisturizers/JER-CC-Hands-3oz-400x500.png?fmt=png-alpha&wid=320"}, 
              {"name":"Jergens Dry Skin", "price": 6.00, "stock":100, "Type of Product": "Lotion", "image":"https://kao-h.assetsadobe3.com/is/image/content/dam/sites/kaousa/www-jergens-com/images/master/products/daily-moisturizers/Hand-and-Body-Cream-thumb.png?fmt=png-alpha&wid=320"}, 
@@ -8,3 +10,5 @@ item_dict = [{"name":"Jergens Smoothing Aloe", "price":5.99, "stock":100, "Type 
              {"name":"Jergens Instant Sun", "price": 4.99, "stock":100, "Type of Product": "Lotion", "image":"https://kao-h.assetsadobe3.com/is/image/content/dam/sites/kaousa/www-jergens-com/images/master/products/natural-glow/56822_JNG_Daily_Group_600x800.png?fmt=png-alpha&wid=320"}]
 
 employee_id = {"Traven": 123, "Alexis": 456, "Abby": 789, "Guest":000}
+
+product_types = ["Lotion", "Soap", "Sponges", "Body Oil" , "Shampoo", "Conditioner", "Deodorant", "Face wash"]
